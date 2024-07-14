45797fd2bf47d6a32a1affcdf83eaf3fe747b349
Ahmed-SaeedM <ahmedsaied6796@gmail.com>
2024-07-14 10:36:10 +0300

    Updating VerifyLinks Method and Solving Problem of SocketTimeOutException

--- src/main/java/uilities/UtilityMethods.java
+++ src/main/java/uilities/UtilityMethods.java
@@ -9,8 +9,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 
 import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.URL;
+import java.net.*;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -35,35 +34,43 @@ public class UtilityMethods {
     public void verifyLinks(List<String> list) {
         for (String link : list) {
             try {
-                URL url = new URL(link);
+                URI uri = new URI(link);
+                URL url = uri.toURL();
                 //create a connection using url object
                 HttpURLConnection httpURLConnection = (HttpURLConnection) url.openConnection();
-                httpURLConnection.setConnectTimeout(2000);
+                httpURLConnection.setConnectTimeout(10000);
+                httpURLConnection.setReadTimeout(10000);
                 httpURLConnection.connect();
                 // use get response code
                 if (httpURLConnection.getResponseCode() == 200) System.out.println(link +
                         "ـــ" + httpURLConnection.getResponseMessage());
                 if (httpURLConnection.getResponseCode() == 404) System.out.println(link +
                         "ـــ" + httpURLConnection.getResponseMessage());
-            } catch (IOException e) {
+//                httpURLConnection.disconnect();
+            } catch (SocketTimeoutException e) {
+                System.err.println("Timeout for URL: " + link);
+            } catch (MalformedURLException e) {
+                System.err.println("Invalid URL: " + link);
+            } catch (IOException | URISyntaxException e) {
                 throw new RuntimeException(e);
             }
         }
     }
 
     public List<WebElement> getAllPageImages() {
-      return driver.findElements(pageImages);
+        return driver.findElements(pageImages);
     }
 
     public void verifyImages(WebElement image) {
         HttpClient client = HttpClientBuilder.create().build();
         HttpGet request = new HttpGet(image.getAttribute("src"));
-            try {
-                HttpResponse response = client.execute(request);
-                if (response.getStatusLine().getStatusCode() == 200) System.out.println("Image With src : " + image.getAttribute("src")+" Is OK");
-                if (response.getStatusLine().getStatusCode() != 200) System.out.println("Broken Image: " + image);
-            } catch (IOException e) {
-                throw new RuntimeException(e);
+        try {
+            HttpResponse response = client.execute(request);
+            if (response.getStatusLine().getStatusCode() == 200)
+                System.out.println("Image With src : " + image.getAttribute("src") + " Is OK");
+            if (response.getStatusLine().getStatusCode() != 200) System.out.println("Broken Image: " + image);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
 
         }
     }
